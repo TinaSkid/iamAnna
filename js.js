5436d19c4418616e01b4559efcb4794e09b929ee
Anna <tina.meow.owo@gmail.com>
2024-12-22 18:24:36 +0100

    Update js.js

---
+++ js.js
@@ -0,0 +1,39 @@
+/*if (window.matchMedia("(max-width: 500px)").matches) {
+  setTimeout(() => {
+    const text = document.getElementById('nazov');
+      text.style.fontSize = '8rem';
+  }, 5000);
+
+  setTimeout(() => {
+    const skrasli = document.getElementById('skrasli');
+      skrasli.style.background = '#02002e';
+  }, 6000);
+} else {
+  setTimeout(() => {
+    const text = document.getElementById('nazov');
+      text.style.fontSize = '6rem';
+  }, 5000);
+}*/
+function zmenaCSS(x) {
+  if (x.matches) {
+    setTimeout(() => {
+      const text = document.getElementById('nazov');
+        text.style.fontSize = '7rem';
+    }, 5000);
+  } else {
+    setTimeout(() => {
+      const text = document.getElementById('nazov');
+        text.style.fontSize = '8rem';
+    }, 5000);
+  
+    setTimeout(() => {
+      const skrasli = document.getElementById('skrasli');
+        skrasli.style.background = '#02002e';
+    }, 6000);
+  }
+}
+var x = window.matchMedia("(max-width: 500px)")
+zmenaCSS(x);
+x.addEventListener("change", function() {
+  zmenaCSS(x);
+});
